768f724b113e27d3c636202f2c71c171c8dbfb3b
Matej Pavic <matej@Matejs-MacBook-Pro.local>
2023-09-12 17:42:10 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { Component } from 'react'
+
+class App extends Component {
+  render() {
+    return (
+      <>
+        <h1>State</h1>
+      </>
+    )
+  }
+}
+
+export default App
